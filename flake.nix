5922a0635b784d5627c2defd9e3d68e14b26276d
Luca Babolin <l.babolgun@gmail.com>
2024-04-14 23:15:54 +0200

    still trying to build on this flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS System";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-colors.url = "github:misterio77/nix-colors";
