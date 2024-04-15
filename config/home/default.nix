bdb1530e76c55a6aaa4229a123b4fadf38007dac
Luca Babolin <l.babolgun@gmail.com>
2024-04-15 12:30:09 +0200

    Added some files

--- config/home/default.nix
+++ config/home/default.nix
@@ -1,11 +1,10 @@
-{ pkgs, config, ... }:
+{ ... }:
 {
   imports = [
     # Enable & Configure Programs
     ./bash.nix
     ./hyprland.nix
     ./kitty.nix
-    ./neofetch.nix
     ./neovim.nix
     ./packages.nix
     ./zsh.nix
